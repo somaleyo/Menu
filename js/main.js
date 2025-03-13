6934c7e16874568ac51dd6c54786e8e10b495c0d
Emil <somaleyoo123@gmail.com>
2025-03-13 14:54:35 +0100

    ajout ombre

---
+++ js/main.js
@@ -0,0 +1,13 @@
+ let style = document.createElement('style');
+
+ let cards = document.querySelectorAll(".card")
+ cards.forEach(cards => {
+    cards.addEventListener('click',()=>{
+      cards.style.boxShadow = "10px 20px 30px rgb(0,0,0)";   
+     
+    })
+
+})
+
+
+    
